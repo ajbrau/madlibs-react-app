c9d4023173f86ebd97ddc4c69dbae5f7eb3208e4
AJ Call <AJCall710@gmail.com>
2018-05-10 12:38:08 -0600

    created madlib form component

---
+++ src/components/madlib_form.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Madlibform extends React.Component {
+    render() {
+      return <h1>Bottega Madlib Form Component</h1>;
+    }
+  }
+
+  export default Madlibform
